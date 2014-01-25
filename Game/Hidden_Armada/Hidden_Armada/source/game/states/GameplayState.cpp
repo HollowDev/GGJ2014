d084a3eb1ee1f1d8aa38752664bcd06f498e5f7d
jonhollow <th3h0ll0w0n3@gmail.com>
2014-01-25 08:47:54 -0500

    Added all the objects to the vcpoj

--- Game/Hidden_Armada/Hidden_Armada/source/game/states/GameplayState.cpp
+++ Game/Hidden_Armada/Hidden_Armada/source/game/states/GameplayState.cpp
@@ -2,8 +2,9 @@
 #include "../../engine/app/WinApp.h"
 #include "../../engine/renderer/D3D9Handler.h"
 #include "../../engine/renderer/TextureManager.h"
-#include "MainMenuState.h"
-#include "../../engine/app/StateSystem.h"
+
+#include "../ObjectManager.h"
+#include "../ObjectFactory.h"
 
 GameplayState::GameplayState( void )
 {
@@ -14,10 +15,32 @@ bool GameplayState::Initialize( WinApp* _app )
 {
 	m_App = _app;
 
-	int p1ID = TextureManager::GetInstance()->LoadTexture(L"assets/textures/ships/Playership1.png");
-	RECT source = {0,0,64,64};
-	m_Player1.Initialize(p1ID,source,32,D3DXVECTOR2(0,0),50,30);
+	int p1ID = TextureManager::GetInstance()->LoadTexture(L"assets/textures/ships/Playership3.png");
+	int weaponID = TextureManager::GetInstance()->LoadTexture(L"assets/textures/weapons/machinegun2.png");
+	RECT source = {0,0,128,128};
 
+	m_Player1 = new Ship();
+	m_Player1->Initialize("assets/data/ships/ship1.txt",D3DXVECTOR2(0,0),weaponID);
+	m_Player1->SetImgID(p1ID);
+	m_Player1->SetImgSource(source);
+
+	p1ID = TextureManager::GetInstance()->LoadTexture(L"assets/textures/ships/Playership2.png");
+	m_Player2 = new Ship();
+	m_Player2->Initialize("assets/data/ships/ship1.txt",D3DXVECTOR2(0,0),weaponID);
+	m_Player2->SetImgID(p1ID);
+	m_Player2->SetImgSource(source);
+
+	ObjectManager::GetInstance()->AddObject(m_Player1,2);
+	ObjectManager::GetInstance()->AddObject(m_Player2,2);
+
+	ObjectFactory::GetInstance()->Initialize();
+	int asteroid = TextureManager::GetInstance()->LoadTexture(L"assets/textures/asteroids/asteroid001.png");
+	for(int i = 0; i < 100; ++i)
+	{
+		ObjectFactory::GetInstance()->Create(&m_AsteroidTest[i], Entity_Asteroid);
+		((Asteroids*)m_AsteroidTest[i])->SetImgID(asteroid);
+		
+	}
 	return true;
 }
 
@@ -28,12 +51,12 @@ void GameplayState::Release( void )
 
 void GameplayState::Render( void )
 {
-	D3D9Handler::m_Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(25, 25, 25), 1.0f, 0);
+	D3D9Handler::m_Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0,0,0), 1.0f, 0);
 	D3D9Handler::m_Device->BeginScene();
 	{
 		D3D9Handler::m_Sprite->Begin( D3DXSPRITE_ALPHABLEND );
 		{
-			m_Player1.Render(0,0);
+			ObjectManager::GetInstance()->Render(0,0);
 		}
 		D3D9Handler::m_Sprite->End();
 	}
@@ -43,7 +66,8 @@ void GameplayState::Render( void )
 
 void GameplayState::Update( float _dt )
 {
-	m_Player1.Update(_dt);
+	ObjectManager::GetInstance()->Update(_dt);
+	ObjectManager::GetInstance()->CheckCollision();
 }
 
 bool GameplayState::Input( void )
@@ -51,12 +75,42 @@ bool GameplayState::Input( void )
 	POINT mouse;
 	GetCursorPos(&mouse);
 	ScreenToClient(m_App->GetHWND(),&mouse);
-	m_Player1.RotateToMouse((int)mouse.x,(int)mouse.y);
+	m_Player1->RotateWeaponToMouse((int)mouse.x,(int)mouse.y);
 
-	if(GetAsyncKeyState(VK_ESCAPE))
+	if(GetAsyncKeyState('W'))
 	{
-		StateSystem::GetInstance()->ChangeState(new MainMenuState());
-		while(GetAsyncKeyState(VK_ESCAPE));
+		m_Player1->SetVel(m_Player1->GetVel() + D3DXVECTOR2(0,-m_Player1->GetMaxSpeed()));
+	}
+	else if(GetAsyncKeyState('S'))
+	{
+		m_Player1->SetVel(m_Player1->GetVel() + D3DXVECTOR2(0,m_Player1->GetMaxSpeed()));
+	}
+
+	if(GetAsyncKeyState('A'))
+	{
+		m_Player1->SetVel(m_Player1->GetVel() + D3DXVECTOR2(-m_Player1->GetMaxSpeed(),0));
+	}
+	else if(GetAsyncKeyState('D'))
+	{
+		m_Player1->SetVel(m_Player1->GetVel() + D3DXVECTOR2(m_Player1->GetMaxSpeed(),0));
+	}
+
+	if(GetAsyncKeyState(VK_UP))
+	{
+		m_Player2->SetVel(m_Player2->GetVel() + D3DXVECTOR2(0,-m_Player2->GetMaxSpeed()));
+	}
+	else if(GetAsyncKeyState(VK_DOWN))
+	{
+		m_Player2->SetVel(m_Player2->GetVel() + D3DXVECTOR2(0,m_Player2->GetMaxSpeed()));
+	}
+
+	if(GetAsyncKeyState(VK_LEFT))
+	{
+		m_Player2->SetVel(m_Player2->GetVel() + D3DXVECTOR2(-m_Player2->GetMaxSpeed(),0));
+	}
+	else if(GetAsyncKeyState(VK_RIGHT))
+	{
+		m_Player2->SetVel(m_Player2->GetVel() + D3DXVECTOR2(m_Player2->GetMaxSpeed(),0));
 	}
 
 	return true;
