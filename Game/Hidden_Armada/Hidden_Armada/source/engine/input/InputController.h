c78bd7768ace9fd4c688fd6a44162d6170fac324
myatckoske <m.yatckoske@gmail.com>
2014-01-25 13:42:44 -0500

    Added Input Manager files
    Added classes to handle user input

---
+++ Game/Hidden_Armada/Hidden_Armada/source/engine/input/InputController.h
@@ -0,0 +1,14 @@
+#ifndef _INPUT_CONTROLLER_H_
+#define _INPUT_CONTROLLER_H_
+#include "InputManager.h"
+
+class InputController
+{
+private:
+
+public:
+	InputController(void);
+	~InputController(void);
+};
+
+#endif
---
+++ Game/Hidden_Armada/Hidden_Armada/source/engine/input/InputManager.h
@@ -0,0 +1,12 @@
+#ifndef _INPUT_MANAGER_H_
+#define _INPUT_MANAGER_H_
+
+class InputManager
+{
+private:
+
+public:
+	InputManager(void);
+	~InputManager(void);
+};
+#endif
