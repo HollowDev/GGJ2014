e5d08b5624c850369c887fa3543f1f7fd7aa0add
jonhollow <th3h0ll0w0n3@gmail.com>
2014-01-25 01:47:23 -0500

    Added a windows.h
    it's a wrapper for the windows directory, with lean and mean.

---
+++ Game/Hidden_Armada/Hidden_Armada/source/engine/windows.h
@@ -0,0 +1,11 @@
+#ifndef _WINDOWS_H_
+#define _WINDOWS_H_
+
+// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN		    
+#define DIRECTINPUT_VERSION 0x0800
+
+// Windows Header Files:
+#include <windows.h>
+
+#endif
