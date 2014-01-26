b4fc9d86f6a70e1b9b36e659e211eff58f164fdb
myatckoske <m.yatckoske@gmail.com>
2014-01-26 08:53:06 -0500

    Fixed the sound issue

--- Game/Hidden_Armada/Hidden_Armada/source/engine/app/WinApp.cpp
+++ Game/Hidden_Armada/Hidden_Armada/source/engine/app/WinApp.cpp
@@ -65,8 +65,9 @@ void WinApp::Initialize( LPCWSTR _title, HINSTANCE _HInstance, BaseState* _start
 	// TODO:: Initialize handlers
 	D3D9Handler::Initialize(m_HWND, _windowed);
 	TextureManager::GetInstance()->Initialize(D3D9Handler::m_Device, D3D9Handler::m_Sprite);
-	AssetManager::GetInstance()->Initialize();
 	SoundManager::GetInstance()->Initialize();
+	AssetManager::GetInstance()->Initialize();
+	
 	StateSystem::GetInstance()->Initialize(this,_startState);
 	m_StateSystem = StateSystem::GetInstance();
 
